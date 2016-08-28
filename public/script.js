0712286ae56b136de81d1221575ad8d910db2dce
Dmetro <Dima.gorlov@gmail.com>
2016-08-28 22:34:29 +0300

    123

--- public/script.js
+++ public/script.js
@@ -3,5 +3,6 @@ angular.module('myApp', ['ui.router'])
     //browserify  ./public/script.js -o ./public/appbundle.js 
     .config(require('./config/router.js'))
     .service('backend_service',require('./service/backend_service.js'))
-    .controller('MainController', require('./controllers/mainController.js'))
+    .controller('mainController', require('./controllers/mainController.js'))
+    .controller('loginController', require('./controllers/loginController.js'))
     
